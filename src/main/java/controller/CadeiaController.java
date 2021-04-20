65dea0d475fd7b07492b4d62e0d974380c254183
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-04-20 22:41:04 +0200

    retorna toda a lista de cadeia

--- src/main/java/controller/CadeiaController.java
+++ src/main/java/controller/CadeiaController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,13 +18,13 @@ import repository.CadeiaRepo;
 public class CadeiaController {
 	@Autowired private CadeiaRepo cadeiaRepo;
 	
-	
+	@RequestMapping("/salvarCadeia")
 	public ModelAndView criarView () {
 		ModelAndView mv = new ModelAndView("/cadeia/Criarcadeia");
 		return mv;
 	}
 	
-	@RequestMapping("/salvarCadeia")
+	@PostMapping("/salvarCadeia")
 	public ModelAndView criar(Cadeia cadeia) {
 		 ModelAndView mv = new ModelAndView("");
 	Cadeia cadeiaExistente = cadeiaRepo.findByNome(cadeia.getNome());
@@ -29,10 +32,20 @@ public class CadeiaController {
 			cadeiaRepo.save(cadeia);
 		}
 		String mensagem = "erro, a cadeira ja existe";
-		mv.addObject(mensagem, "mensagem");
+		mv.addObject("cadeia", mensagem);
 		return mv;
 		
 	}
+	
+	@RequestMapping("listaCadeias")
+	public ModelAndView Listar() {
+		ModelAndView mv = new ModelAndView("/cadeia/listaCadeias");
+		List<Cadeia> cadeia = cadeiaRepo.findAll();
+		return mv.addObject("cadeia", cadeia);
+	}
+	
+	
+	
 	
 	@RequestMapping("/actualizarCadeia")
 	public ModelAndView ViewActualizar () {
@@ -47,7 +60,6 @@ public class CadeiaController {
 		
 		 Cadeia cadeiaAntiga = cadeiaRepo.findById(id);
 		 BeanUtils.copyProperties(cadeiaNova, cadeiaAntiga);
-		 
 		 cadeiaRepo.save(cadeiaAntiga);
 		 
 		return mv;
